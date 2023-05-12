75c8ce32d6cafe696359fde5b40ddb74609c1fa3
cu-kai <13281185+cu-kai@users.noreply.github.com>
2023-05-12 20:37:25 +0100

    Sudden Death

--- src/cgame/cg_local.h
+++ src/cgame/cg_local.h
@@ -1679,6 +1679,8 @@ struct cgs_t
 	float    momentumHalfLife; // used for momentum bar (un)lock markers
 	float    unlockableMinTime;  // used for momentum bar (un)lock markers
 
+	float    suddenDeathDrillCount;
+	float    suddenDeathLeechCount;
 	float    buildPointBudgetPerMiner;
 	float    buildPointRecoveryInitialRate;
 	float    buildPointRecoveryRateHalfLife;
