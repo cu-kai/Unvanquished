c7c68c26a3efb3db5732ac4909d7b4563f653ecc
cu-kai <13281185+cu-kai@users.noreply.github.com>
2024-07-09 18:44:42 +0100

    sg_admin.cpp: setrotation command

--- src/sgame/sg_admin.cpp
+++ src/sgame/sg_admin.cpp
@@ -399,6 +399,12 @@ static const g_admin_cmd_t     g_admin_cmds[] =
 	},
 
 	{
+		"setrotation",     G_admin_setrotation,    false, "setrotation",
+		N_("sets the current rotation"),
+		N_("[^3rotation name^7]")
+	},
+
+	{
 		"showbans",     G_admin_showbans,    true,  "showbans",
 		N_("display a (partial) list of active bans"),
 		N_("(^5name|IP(/mask)^7) (^5start at ban#^7)")
@@ -542,7 +548,7 @@ static int G_admin_stealthed( gentity_t *admin )
 {
 	/*
 		2 = super stealth (admin is anonymous)
-		1 = standard stealth (use registered (admin) name, not netname. 
+		1 = standard stealth (use registered (admin) name, not netname.
 		                      this was the previous default behaviour)
 		0 = not stealthed
 	*/
@@ -551,7 +557,7 @@ static int G_admin_stealthed( gentity_t *admin )
 		return ( g_adminStealthConsole.Get() ? 2 : 0 );
 	}
 
-	if ( G_admin_permission( admin, ADMF_SUPERSTEALTH ) 
+	if ( G_admin_permission( admin, ADMF_SUPERSTEALTH )
 	     || g_adminStealthMode.Get() == 2 )
 	{
 		return 2;
@@ -567,7 +573,7 @@ static int G_admin_stealthed( gentity_t *admin )
 }
 
 template <typename... Args>
-void G_admin_action( const char *action, const char *translation, 
+void G_admin_action( const char *action, const char *translation,
                      gentity_t *admin, Args... args )
 {
 	char cloakTags[][8] = { "", "^9●^*", "^0●^*" };
@@ -579,7 +585,7 @@ void G_admin_action( const char *action, const char *translation,
 	// quote only has a limited buffer, so cache these values here.
 	Q_strncpyz( qAdminNetName, Quote( G_user_name( admin, "console" ) ), sizeof( qAdminNetName ) );
 	Q_strncpyz( qAdminTaggedName, Quote( va( "%s%s", cloakTags[ G_admin_stealthed( admin ) ], G_user_name( admin, "console" ) ) ), sizeof( qAdminTaggedName ) );
-	Q_strncpyz( qAdminAdminName, Quote( ( admin ) 
+	Q_strncpyz( qAdminAdminName, Quote( ( admin )
 	                                    ? ( admin->client->pers.admin ? admin->client->pers.admin->name : qAdminNetName )
 	                                    : "console" ), sizeof( qAdminAdminName ) );
 	Q_strncpyz( qAdminStealthName, Quote( g_adminStealthName.Get().c_str() ), sizeof( qAdminStealthName ) );
@@ -600,9 +606,9 @@ void G_admin_action( const char *action, const char *translation,
 	*/
 
 	// tell the console what happened first
-	trap_SendServerCommand( -2, va( "print_tr %s %s", action, 
-	                        va( translation, 
-	                            qAdminTaggedName, 
+	trap_SendServerCommand( -2, va( "print_tr %s %s", action,
+	                        va( translation,
+	                            qAdminTaggedName,
 	                            args... ) ) );
 
 	for ( int i = 0; i < level.maxclients; i++ )
@@ -628,9 +634,9 @@ void G_admin_action( const char *action, const char *translation,
 			name = qAdminTaggedName;
 		}
 
-		trap_SendServerCommand( i, va( "print_tr %s %s", action, 
-		                        va( translation, 
-		                            name.c_str(), 
+		trap_SendServerCommand( i, va( "print_tr %s %s", action,
+		                        va( translation,
+		                            name.c_str(),
 		                            args... ) ) );
 	}
 }
@@ -1718,8 +1724,8 @@ static void G_admin_ban_message(
 
 		Com_sprintf( areason, alen,
 		             "^3Banned player ^*%s^3 tried to connect from %s (ban #%d)",
-		             ( !Q_stricmp( name, ban->name ) 
-		               ? ban->name 
+		             ( !Q_stricmp( name, ban->name )
+		               ? ban->name
 		               : va( "%s ^3(a.k.a. ^*%s^3)", name, ban->name ) ),
 		             ent->client->pers.ip.str,
 		             ban->id );
@@ -2628,6 +2634,41 @@ bool G_admin_setlevel( gentity_t *ent )
 	return true;
 }
 
+extern mapRotations_t mapRotations;
+
+bool G_admin_setrotation( gentity_t *ent )
+{
+	char new_rotation[ MAX_NAME_LENGTH ];
+
+	if ( trap_Argc() < 2 ) {
+		ADMP( QQ( N_( "^3setrotation:^* usage: setrotation [rotation name]\n" ) ) );
+		ADMP( QQ( N_( "Available rotations:\n" ) ) );
+		goto rotationlist;
+	}
+
+	trap_Argv( 1, new_rotation, sizeof( new_rotation ) );
+
+	for ( int i = 0; i < mapRotations.numRotations; i++ ) {
+		if ( Q_stricmp( mapRotations.rotations[i].name, new_rotation ) == 0 ) {
+			G_StartMapRotation( new_rotation, false, false, true, 0 );
+			G_admin_action( QQ( N_( "^3setrotation:^* rotation ^3$2$^* was started by $1$" ) ), "%s %s", ent, Quote( new_rotation ) );
+			return true;
+		}
+	}
+	ADMP( QQ( N_( "^3setrotation:^* rotation not found. Available rotations:" ) ) );
+	goto rotationlist;
+	rotationlist:
+	{
+		ADMBP_begin();
+		for ( int i = 0; i < mapRotations.numRotations; i++ ) {
+			ADMBP( va( "    %s", mapRotations.rotations[i].name ) );
+		}
+		ADMBP_end();
+		ADMP( va( "%s %d", QQ( N_( "\nNumber of available rotations: ^3$1$" ) ), mapRotations.numRotations ) );
+	}
+	return false;
+}
+
 bool G_admin_slap( gentity_t *ent )
 {
 	int            pid;
@@ -2717,7 +2758,7 @@ bool G_admin_slap( gentity_t *ent )
 
 	if ( health->Alive() )
 	{
-		G_admin_action( QQ( N_( "^3slap:^* $1$^* slapped $2$ ^*$3$" ) ), "%s %s %s", ent, Quote( vic->client->pers.netname ), 
+		G_admin_action( QQ( N_( "^3slap:^* $1$^* slapped $2$ ^*$3$" ) ), "%s %s %s", ent, Quote( vic->client->pers.netname ),
 		                ( damage > 0 ? Quote( va( "with %.0f damage", damage ) ) : QQ( "" ) ) );
 
 	} // only print the chat msg if they don't die. otherwise the MOD_SLAP event will suffice.
@@ -3232,7 +3273,7 @@ bool G_admin_unban( gentity_t *ent )
 	               ban->ip.str ) );
 
 	if ( expireOnly )
-	{		
+	{
 		G_admin_action( QQ( N_("^3unban:^* ban #$2$ for $3$^* has been expired by $1$") ),
 		                "%s %d %s", ent, bnum, Quote( ban->name ) );
 
@@ -3422,7 +3463,7 @@ bool G_admin_adjustban( gentity_t *ent )
 	                Quote( ban->name ),
 	                ( mask ) ? Quote( va( "netmask: /%d%s", mask, ( length >= 0 || *reason ) ? ", " : "" ) ) : QQ( "" ),
 	                ( length >= 0 ) ? QQ( N_( "duration: " ) ) : QQ( "" ),
-	                ( atoi( seconds ) > 0 ) ? Quote( seconds ) : QQ( "" ), 
+	                ( atoi( seconds ) > 0 ) ? Quote( seconds ) : QQ( "" ),
 	                ( atoi( seconds ) > 0 ) ? duration : QQ( "" ),
 	                ( length >= 0 && *reason ) ? ", " : QQ( "" ),
 	                ( *reason ) ? QQ( N_( "reason: ") ) : QQ( "" ),
@@ -3582,7 +3623,7 @@ bool G_admin_speclock( gentity_t *ent )
 	else
 	{
 		G_admin_action( QQ( N_("^3speclock:^* $1$^* blocked team-change for $2$^* for $3$$4t$") ),
-		                "%s %s %s %s", ent, Quote( vic->client->pers.netname ), Quote( time ), 
+		                "%s %s %s %s", ent, Quote( vic->client->pers.netname ), Quote( time ),
 		                ( lockTime ? duration : QQ( "" ) ) );
 	}
 
@@ -4229,7 +4270,7 @@ static int ban_out( void *ban, char *str )
 		d_color = Color::Green;
 	}
 
-	Com_sprintf( str, MAX_STRING_CHARS, 
+	Com_sprintf( str, MAX_STRING_CHARS,
 	             "^3------------------------------\n"
 	             "    Name:     %s\n"
 	             "    IP:       %s%s\n"
@@ -4237,12 +4278,12 @@ static int ban_out( void *ban, char *str )
 	             "    Reason:   ^3%s\n"
 	             "    Date:     ^3%s\n"
 	             "    Details:  %s%s%s%s%s%s",
-	             b->name, 
+	             b->name,
 	             Color::ToString( ( strchr( b->ip.str, '/' ) ) ? Color::Red : Color::Yellow ).c_str(),
 	             b->ip.str,
 	             b->banner,
 	             b->reason,
-	             date, 
+	             date,
 	             Color::ToString( G_ADMIN_BAN_IS_WARNING( b ) ? Color::Yellow : Color::Red ).c_str(),
 	             G_ADMIN_BAN_IS_WARNING( b ) ? "WARNING " : "BAN ",
 	             ( ( active && b->expires ) ? "^7- expiring in " : "^7- " ),
@@ -4365,7 +4406,7 @@ bool G_admin_adminhelp( gentity_t *ent )
 		ADMP( va( "%s %d", QQ( N_("^3adminhelp:^* $1$ available commands\n"
 		"run adminhelp [^3command^7] for adminhelp with a specific command.") ),count ) );
 
-		if ( G_admin_permission( ent, ADMF_STEALTH ) 
+		if ( G_admin_permission( ent, ADMF_STEALTH )
 		     || G_admin_permission( ent, ADMF_SUPERSTEALTH ) )
 		{
 			ADMP( QQ( N_( "\n^3adminhelp:^* You are cloaked by Admin Stealth\n"
@@ -5609,7 +5650,7 @@ bool G_admin_buildlog( gentity_t *ent )
 
 	if ( ent && ent->client->pers.team != TEAM_NONE )
 	{
-		G_admin_action( QQ( N_("^3buildlog:^* $1$^* requested a log of recent$2$ building activity") ), 
+		G_admin_action( QQ( N_("^3buildlog:^* $1$^* requested a log of recent$2$ building activity") ),
 		                "%s %s", ent,
 		                ( team != TEAM_NONE ? Quote( va( " %s", BG_TeamName( team ) ) ) : QQ( "" ) ) );
 	}
@@ -6122,11 +6163,11 @@ static bool BotAddCmd( gentity_t* ent, const Cmd::Args& args )
 		if ( ent )
 		{
 			G_admin_action( QQ( N_("^3bot:^* $1$^* added a bot to the $2$ team with behavior ^3$3$$4$") ),
-			                "%s %s %s %s", 
-			                ent, 
-			                Quote( va( "%s%s^*", 
-			                           ( team == TEAM_ALIENS ? "^i" : "^d" ), 
-			                           BG_TeamName( team ) ) ), 
+			                "%s %s %s %s",
+			                ent,
+			                Quote( va( "%s%s^*",
+			                           ( team == TEAM_ALIENS ? "^i" : "^d" ),
+			                           BG_TeamName( team ) ) ),
 			                Quote( behavior ),
 			                ( skill ) ? Quote( va( "^* and skill ^3%d^*", skill ) ) : QQ( "" ) );
 		}
@@ -6318,7 +6359,7 @@ bool G_admin_bot( gentity_t *ent )
 
 			if ( ent )
 			{
-				G_admin_action( QQ( N_("^3bot:^* $1$^* removed bot $2$^* from the game") ), "%s %s", 
+				G_admin_action( QQ( N_("^3bot:^* $1$^* removed bot $2$^* from the game") ), "%s %s",
 				                ent, G_quoted_user_name( &g_entities[ clientNum ], "a bot" ) );
 			}
 			G_BotDel( clientNum ); //delete the bot
@@ -6452,7 +6493,7 @@ bool G_admin_bot( gentity_t *ent )
 			}
 
 			G_admin_action( QQ( N_("^3bot:^* $1$^* set bot skill for $2$ to ^3$3$") ),
-			                    "%s %s %d", ent, 
+			                    "%s %s %d", ent,
 			                    Quote( nameStr ),
 			                    skill );
 		}
